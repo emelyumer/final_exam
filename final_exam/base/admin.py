d9990c88621edcd7f51d594f83ada181b0aa6e33
emelyumer <emi_92rs@abv.bg>
2024-04-06 02:35:08 +0300

    set up

--- final_exam/base/admin.py
+++ final_exam/base/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin, get_user_model
+from final_exam.base.models import Product, Review, Order, OrderItem, ShippingAddress
+UserModel = get_user_model()
 
-# Register your models here.
+
+@admin.register(UserModel)
+class AppUserAdmin(auth_admin.UserAdmin):
+    model = UserModel
+
+    list_display = ('pk', 'email', 'is_staff', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('pk',)
+
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    pass
+
+@admin.register(Review)
+class AdminReview(admin.ModelAdmin):
+    pass
+
+@admin.register(Order)
+class AdminOrder(admin.ModelAdmin):
+    pass
+
+@admin.register(OrderItem)
+class AdminOrderItem(admin.ModelAdmin):
+    pass
+
+@admin.register(ShippingAddress)
+class AdminShippingAddress(admin.ModelAdmin):
+    pass
